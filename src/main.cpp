8f57f46c22e00106faf028d1a5e47c1c3bd7611b
dorythecat <pemegomachado@gmail.com>
2025-09-24 15:47:41 +0200

    Put the new operator to use

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "matrix.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const Matrix<2, 3, int> mat1(5);
+
+    std::cout << "Matrix 1:\n" << mat1 << std::endl;
     return 0;
 }
