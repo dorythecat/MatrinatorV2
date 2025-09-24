c58559a5497b40041591fffa1b44456437ba41ac
dorythecat <pemegomachado@gmail.com>
2025-09-24 15:40:55 +0200

    Added basic matrix class

---
+++ src/matrix.h
@@ -0,0 +1,35 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+template<unsigned int Rows, unsigned int Cols, typename T>
+class Matrix {
+public:
+    explicit Matrix(T value = T()) { for (unsigned int i = 0; i < Rows * Cols; ++i) data[i] = value; }
+    ~Matrix() { free(data); }
+
+    Matrix(const Matrix& other) {
+        memcpy(data, other.data, sizeof(T) * Rows * Cols);
+    }
+    Matrix& operator=(const Matrix& other) {
+        if (this != &other) memcpy(data, other.data, sizeof(T) * Rows * Cols);
+        return *this;
+    }
+
+    T& operator()(const unsigned int row, const unsigned int col) { return data[row * Cols + col]; }
+    const T& operator()(const unsigned int row, const unsigned int col) const { return data[row * Cols + col]; }
+
+
+    [[nodiscard]] static unsigned int rows() { return Rows; }
+    [[nodiscard]] static unsigned int cols() { return Cols; }
+
+    bool operator==(const Matrix& other) const {
+        if (this == &other) return true;
+        return memcmp(data, other.data, sizeof(T) * Rows * Cols) == 0;
+    }
+    bool operator!=(const Matrix& other) const { return !(*this == other); }
+
+private:
+    T data[Rows * Cols];
+};
+
+#endif // MATRIX_H
