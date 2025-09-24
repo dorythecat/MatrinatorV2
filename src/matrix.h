6b5cee2eafded3efd694de1f80d83bc90bdc2680
dorythecat <pemegomachado@gmail.com>
2025-09-24 15:53:36 +0200

    Added set function to matrix class

--- src/matrix.h
+++ src/matrix.h
@@ -26,6 +26,12 @@ public:
         return data[row * Cols + col];
     }
 
+    // Element assignment
+    void set(const unsigned int row, const unsigned int col, const T& value) {
+        if (row >= Rows || col >= Cols) throw std::out_of_range("Matrix - Index out of range");
+        data[row * Cols + col] = value;
+    }
+
     // Size information
     [[nodiscard]] static unsigned int rows() { return Rows; }
     [[nodiscard]] static unsigned int cols() { return Cols; }
