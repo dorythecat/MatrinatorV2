d58733772ae0a5b5e4b145f651e97162e036d2bd
dorythecat <pemegomachado@gmail.com>
2025-09-24 16:09:33 +0200

    Removed stupid buggy functions :(

--- src/matrix.h
+++ src/matrix.h
@@ -6,15 +6,6 @@ class Matrix {
 public:
     // Basic definitions
     explicit Matrix(T value = T()) { for (unsigned int i = 0; i < Rows * Cols; i++) data[i] = value; }
-    ~Matrix() { free(data); }
-
-    Matrix(const Matrix& other) {
-        memcpy(data, other.data, sizeof(T) * Rows * Cols);
-    }
-    Matrix& operator=(const Matrix& other) {
-        if (this != &other) memcpy(data, other.data, sizeof(T) * Rows * Cols);
-        return *this;
-    }
 
     // Element access with bounds checking
     T& operator()(const unsigned int row, const unsigned int col) {
