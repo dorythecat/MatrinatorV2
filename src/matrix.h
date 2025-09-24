ad04f3d5dee8694ac91ecf4129bec049f9750e7c
dorythecat <pemegomachado@gmail.com>
2025-09-24 15:48:28 +0200

    Added a few useful comments

--- src/matrix.h
+++ src/matrix.h
@@ -4,6 +4,7 @@
 template<unsigned int Rows, unsigned int Cols, typename T>
 class Matrix {
 public:
+    // Basic definitions
     explicit Matrix(T value = T()) { for (unsigned int i = 0; i < Rows * Cols; ++i) data[i] = value; }
     ~Matrix() { free(data); }
 
@@ -15,19 +16,22 @@ public:
         return *this;
     }
 
+    // Element access
     T& operator()(const unsigned int row, const unsigned int col) { return data[row * Cols + col]; }
     const T& operator()(const unsigned int row, const unsigned int col) const { return data[row * Cols + col]; }
 
-
+    // Size information
     [[nodiscard]] static unsigned int rows() { return Rows; }
     [[nodiscard]] static unsigned int cols() { return Cols; }
 
+    // Comparison
     bool operator==(const Matrix& other) const {
         if (this == &other) return true;
         return memcmp(data, other.data, sizeof(T) * Rows * Cols) == 0;
     }
     bool operator!=(const Matrix& other) const { return !(*this == other); }
 
+    // Output
     friend std::ostream& operator<<(std::ostream& os, Matrix m) {
         for (unsigned int i = 0; i < Rows; ++i) {
             for (unsigned int j = 0; j < Cols; ++j) {
