1098006c4e953a32580d2ab5ff3a69881c844017
dorythecat <pemegomachado@gmail.com>
2025-09-28 12:39:12 +0200

    Marginally improved main function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "matrix.h"
 
 int main() {
-    const Matrix<float, 3> mat1(std::function<int(size_t, size_t)>([](size_t i, size_t j) {
+    const Matrix<float, 3> mat1(std::function([](const size_t i, const size_t j) {
         return static_cast<int>(i / (j + 1) - j * i + 1);
     }));
     std::cout << "Matrix 1:\n" << mat1 << std::endl;
